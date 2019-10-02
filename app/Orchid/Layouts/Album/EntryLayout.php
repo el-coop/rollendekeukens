e2f13b3a1a01cb2e792ed53ab2a11d8c60210d39
Nur Bar <LCD344@yahoo.com>
2019-10-02 13:23:37 -0300

    Added reordering to thumbnail gallery + started working on entry gallery

---
+++ app/Orchid/Layouts/Album/EntryLayout.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Orchid\Layouts\Album;
+
+use Orchid\Screen\Field;
+use Orchid\Screen\Layouts\Rows;
+
+class EntryLayout extends Rows
+{
+    /**
+     * Views.
+     *
+     * @return Field[]
+     */
+    protected function fields(): array
+    {
+        return [];
+    }
+}
