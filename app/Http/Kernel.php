87616d1ef52f981b659246df4a7564602773205c
Cucurrucho <adam.bar91@gmail.com>
2019-10-17 00:05:52 -0300

    removed platform middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,10 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-        ],
-		'platform' => [
-			LocaleMiddleware::class
-		]
+        ]
     ];
 
     /**
