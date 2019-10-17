302892dc9b812f203056fc464b6c632538d0d503
Nur Bar <LCD344@yahoo.com>
2019-10-16 23:36:24 -0500

    removing unnecessary code

--- resources/js/platform/platform.js
+++ resources/js/platform/platform.js
@@ -6,9 +6,3 @@ import TinyMCE5 from "./controllers/TinyMCE5";
 application.register("fields--image-upload", ImageUploadController);
 application.register("layouts--entry-options", EntryOptions);
 application.register("fields--tinymce5", TinyMCE5);
-
-$(document).on('focusin', function (e) {
-    if ($(e.target).closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
-        e.stopImmediatePropagation();
-    }
-});
