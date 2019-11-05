b1319971bccba579afa59c8370ee215c91b63276
Nur Bar <LCD344@yahoo.com>
2019-11-05 10:37:20 -0600

    added versioning for assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/platform/platform.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/platform/platform.scss', 'public/css');
+    .sass('resources/sass/platform/platform.scss', 'public/css')
+    .version();
