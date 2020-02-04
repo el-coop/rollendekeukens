893814dbc326d49e289cabe73a43f82b1e08d87a
Nur Bar <LCD344@yahoo.com>
2020-02-04 12:04:09 -0300

    fixing link

--- resources/views/partials/bottomAlbum.blade.php
+++ resources/views/partials/bottomAlbum.blade.php
@@ -1,4 +1,4 @@
 <p class="mt-3 pt-1 title is-4">
-    <a class="has-text-red" href="{{ $settings->get('instagram','') }}">#RollendeKeukens</a>
+    <a class="has-text-red" href="{{  $settings->get('facebook','') }}">#RollendeKeukens</a>
 </p>
 <bottom-album @open-entry="modalData = $event" :entries="{{$bottomEntries->sortBy('order')->values()}}"></bottom-album>
